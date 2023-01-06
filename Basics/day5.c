e994d596b0825c73115e713f4ed1de96e1fb3e55
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-06 13:07:41 +0000

    user define value

---
+++ Basics/day5.c
@@ -0,0 +1,17 @@
+// multiplication of two number - taken by user
+
+#include <stdio.h>
+
+int main() {
+    
+    float x;
+    printf("enter x value :  ");
+    scanf("%f",&x);
+    float y;
+    printf("\n enter y value :  ");
+    scanf("%f",&y);
+    
+    float XY = x*y;
+    printf("\n multiplication of x and y : %f",XY);
+    return 0;
+}
---
+++ Basics/day6.c
@@ -0,0 +1,14 @@
+// area of circle - user define radius
+
+#include <stdio.h>
+
+int main() {
+    
+   float radius;
+   printf("Enter radius : ");
+   scanf("%f",&radius);
+   float pi = 3.14;
+   float area = pi*radius*radius;
+   printf("area of circle : %f",area);
+    return 0;
+}
