1849c8ceae53986670646463689d8cab9e8d614b
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-07 16:37:08 +0000

    Sum of two number

---
+++ Basics/sum_num.c
@@ -0,0 +1,19 @@
+// SUM of two number, teken user
+
+#include <stdio.h>
+
+int main() {
+    
+    int x;
+    printf("Enter the First Number :  ");
+    scanf("%d",&x);
+    
+    int y;
+    printf("\nEnter the Second Number :  ");
+    scanf("%d",&y);
+    
+    int sum = x+y;
+    printf("\nSum of the two Number : %d",sum);
+    
+     return 0;
+}
