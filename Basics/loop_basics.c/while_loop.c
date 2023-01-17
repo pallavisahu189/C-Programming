844f4adfdad90ecaffda182b2896a091f52099e3
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-17 13:03:46 +0000

    using while loop

---
+++ Basics/loop_basics.c/while_loop.c
@@ -0,0 +1,12 @@
+//predict the output 
+
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    while(i<=100){        //jab tak i<=100 hai tab tak print krate rho
+     printf("%d",i++);   
+    }
+    
+   return 0;
+}
