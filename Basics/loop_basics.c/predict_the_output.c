8d8a8f81c8162dd99e89a32eaafcd4885beaf9b7
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-17 13:04:30 +0000

    predict the output program

---
+++ Basics/loop_basics.c/predict_the_output.c
@@ -0,0 +1,12 @@
+// predict the output
+
+#include <stdio.h>
+
+int main() {
+    int x = 4,y,z;
+    y = --x;
+    z = x--;
+    printf("%d\n%d\n%d\n",x,y,z);
+    
+    return 0;
+}
---
+++ Basics/loop_basics.c/predict_the_output1.c
@@ -0,0 +1,12 @@
+//predict the output
+
+#include <stdio.h>
+
+int main() {
+    int x=4, y=3, z;
+    z = x-- -y;
+   
+    printf("%d\n%d\n%d\n",x,y,z);
+    
+    return 0;
+}
