8d0d201af498f9424bb4ec6264ac736a6c95ae36
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-18 17:25:16 +0000

    WAP to count digits of a given number.

---
+++ Basics/loop_basics.c/count_digit.c
@@ -0,0 +1,18 @@
+//Ques : WAP to count digits of a given number.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d",n);
+    
+    int count = 0;
+    while(n!=0){
+        n=n/10;
+        count++;
+    }
+        printf("The on o digits are %d", count);
+ 
+    return 0;
+}
