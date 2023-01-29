3b3fccdd5b2201c88b7843da8537074ad032d2e4
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-29 18:40:03 +0000

    Print the given pattern

---
+++ Basics/pattern_programmimg/pattern.c
@@ -0,0 +1,24 @@
+// pattern printing
+//HW : Print the given pattern
+
+//****
+//****
+//****
+//****
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    
+    for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
