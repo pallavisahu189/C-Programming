cf52bf88562721476a4525c2ded58a5ed67bbd9c
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-02-12 17:47:49 +0000

    function

---
+++ Basics/function/function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void greed(){
+    printf("Good Morning\n");
+    printf("How are you?\n");
+    return;
+}
+
+int main(){
+    greed();
+    greed();
+    greed();
+    return 0;
+}
