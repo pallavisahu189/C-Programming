b357219e4f072ea9e24a4329820cf64d023236a5
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-02-19 12:23:53 +0000

    print array

---
+++ Basics/array/arr_print.c
@@ -0,0 +1,12 @@
+// 5 6 4 3 2
+// 3 
+
+#include <stdio.h>
+int main(){
+    int arr[5];
+    for(int i=0; i<=4; i++){
+          scanf("%d",&arr[i]);
+    }
+    printf("%d ",arr[3]);
+    return 0;
+}
